90ea0a14c6902300900c39764aa1e5cea34937eb
Evan Troget <contact.evantroget@gmail.com>
2026-03-24 09:51:03 +0100

    homepage

--- assurmoi/app.js
+++ assurmoi/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+const PORT = process.env.PORT || 3000;
+
+app.use('/', () => (
+    console.log('Homepage')
+))
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
+module.exports = app;
